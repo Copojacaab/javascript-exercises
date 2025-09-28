e43eadbe73aaa5568a070f297cc8aa39fe919bf0
Copojaab <palermijacopo@gmail.com>
2025-09-28 22:26:46 +0200

    7 and 8 done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // trasformo la stringa in array
+    let strToArr = str.split('')
+    strToArr.reverse();
+    let result = strToArr.join('');
+    return result;
 };
 
+console.log(reverseString('gianni'))
 // Do not edit below this line
 module.exports = reverseString;
