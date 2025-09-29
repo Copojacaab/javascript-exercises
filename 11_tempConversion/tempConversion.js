c4408505b821c2449e2f95bd6ca78797cc149922
Copojaab <palermijacopo@gmail.com>
2025-09-29 09:56:59 +0200

    "Finiti tutti gli esercizi della sezione loops and array"

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenightTemperature) {
+  let celsiusConversion =  (farenightTemperature - 32) * (5 / 9);
+  celsiusConversion = Math.floor(celsiusConversion * 10) / 10;
+  return celsiusConversion;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature) {
+  let farenightConversion = (celsiusTemperature * (9/5)) + 32;
+  farenightConversion = Math.floor(farenightConversion * 10) / 10;
+  return farenightConversion;
 };
 
+console.log(convertToFahrenheit(73.2));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
