06b6c5163ab0296ea9380489f3fe04089e662f84
Copojaab <palermijacopo@gmail.com>
2025-10-01 13:39:11 +0200

    palindrome exercise done. EASYYYY UOMOOO!

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const { lowerCase } = require("case-anything");
 
+const palindromes = function (string) {
+    // per togliere punteggiatura e spazi
+    string = string.replace(/[\s\p{P}]/gu, "");
+    string = lowerCase(string);
+    // strigna pronta per l'analisi 
+    let arrStr = string.split('');
+    console.log(arrStr);
+    const length = arrStr.length;
+    for(let i = 0; i < length - 1; i++){
+        if(arrStr[i] !== arrStr[length - i - 1]){
+            return false;
+        }
+    }
+    return true;
 };
-
+console.log(palindromes('cac'));
 // Do not edit below this line
 module.exports = palindromes;
