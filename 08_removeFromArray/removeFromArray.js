6a73c324336ff73522155cef3f494b4401bf9320
Copojaab <palermijacopo@gmail.com>
2025-10-01 11:49:59 +0200

    mi sono scordato il commit non mi ricordo una sega

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rmElements) {
+    const ERROR = 'Puoi rimuovere solo interi';
+    const NOT_FOUND = 'Elemento non presente nell\'array';
+    for(let i = 0; i < rmElements.length; i++){
+        let numberToRm = rmElements[i];
+        // per scorrere l'arr da modificare
+        for(let j = arr.length-1; j >= 0; j--){
+            if(arr[j] === numberToRm){
+                arr.splice(j, 1);
+            }
+        }
+    }
+    return arr;
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 7, "tacos")); 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    if(firstNumber < 0 || secondNumber < 0)
+        return 'ERROR';
 
-};
+    if(!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)){
+        return 'ERROR';
+    }
+    let max = Math.max(firstNumber, secondNumber);
+    let min = Math.min(firstNumber, secondNumber);
 
+    let sum = 0;
+    for(let i = min; i <= max; i++){
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(sumAll(1,10));
 // Do not edit below this line
 module.exports = sumAll;
