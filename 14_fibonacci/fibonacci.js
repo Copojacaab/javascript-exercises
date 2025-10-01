dc8031942e8912f0c18cdcd0c26ca0a41429e577
Copojaab <palermijacopo@gmail.com>
2025-10-01 14:18:32 +0200

    fibonacci done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibStep = Number(num);
+    if(Number.isNaN(fibStep))
+        return 'ERROR';
+    if(fibStep < 0) 
+        return 'OOPS';
+    if (fibStep === 0)
+        return 0;
 
+    let i;
+    var fib = [0,1];
+
+    for(i = 2; i <= fibStep; i++){
+        // next fibonacci = previous + one before
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+
+    return fib[fib.length-1];
 };
 
+console.log(fibonacci(6));
 // Do not edit below this line
 module.exports = fibonacci;
