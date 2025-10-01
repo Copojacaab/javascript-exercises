ec39600736c5e8b1df2b43588b1ef2f29a984ab4
Copojaab <palermijacopo@gmail.com>
2025-10-01 12:17:44 +0200

    add, sum, subsctraction, multiply, power done

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,27 +1,49 @@
-const add = function() {
-	
+// sum of two n
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+// subtraction of two n
+const subtract = function(n1, nToSub) {
+	return n1 - nToSub;
 };
 
-const sum = function() {
-	
+// sum of the number in an arr
+const sum = function(arr) {
+  let sum = 0;
+  arr.forEach(num => {
+    sum += num;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+// multiply all the n in an arr
+const multiply = function(arr) {
+  if(arr.length === 0){
+    return 0;
+  }
+  let product = 1;
+  arr.forEach(num => {
+    product = product * num;
+  });
+  return product;
 };
 
-const power = function() {
-	
+// take base and exponent and returns the power
+const power = function(base, exp) {
+  let result = 1;
+  for(let i = 0; i < exp; i++){
+    result *= base;
+  }
+  return result;
 };
 
+// returns the factorial of a number
 const factorial = function() {
 	
 };
 
+console.log(multiply([1,2]));
 // Do not edit below this line
 module.exports = {
   add,
