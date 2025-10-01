f1ab0738ba44f9b6290bfd21e93d8db17b059e43
Copojaab <palermijacopo@gmail.com>
2025-10-01 14:36:21 +0200

    base mechanics functions ok

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let currOldest;
+    let currMaxAge = 0;
+    arr.forEach(element => {
+        let age = element.yearOfDeath - element.yearOfBirth;
+        if (age > currMaxAge){
+            currMaxAge = age;
+            currOldest = element.name;
+        }
+    });
+    return currOldest.name;
 };
 
+    const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        // yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+
+console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
