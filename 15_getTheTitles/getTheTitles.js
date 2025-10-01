71a23e1be4cd5d079ab0aa6ee92c4b5075cb02ce
Copojaab <palermijacopo@gmail.com>
2025-10-01 14:22:33 +0200

    get the titles done, easy

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrToConvert) {
+    let arrConverted = [];
+    arrToConvert.forEach(element => {
+        arrConverted.push(element.title);
+    });
+    return arrConverted;
 };
 
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
